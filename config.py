0889709683e9f7423c8291dce981bc97fa2402e9
mezoro-code <sologameacg@gmail.com>
2025-01-09 22:33:29 +0600

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}\n\n<b>ꜱᴇɴᴘᴀɪ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜꜱᴇ ᴘʟᴇᴀꜱᴇ ʙᴇ ɢᴇɴᴛʟᴇ\n\nᴘʟᴇᴀꜱᴇ ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ ᴛᴏᴏ ᴜꜱᴇ ᴍᴇ ᴀꜱ ᴍᴜᴄʜ ʏᴏᴜ ᴡᴀɴᴛ.ɪ ᴡᴇ'ʟʟ ᴛʀʏ ᴍʏ ʙᴇꜱᴛ ᴛᴏ ꜱᴇᴛɪꜱꜰʏ ʏᴏᴜ</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
