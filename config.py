efd5c08e06c5411bb206db93260504327949c74e
mezoro-code <sologameacg@gmail.com>
2025-01-09 19:17:47 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+#(©)CodeXBotz
+
 import os
 import logging
 from dotenv import load_dotenv
@@ -5,54 +7,51 @@ from logging.handlers import RotatingFileHandler
 
 load_dotenv()
 
-# Bot token @Botfather
+#Bot token @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
-# Your API ID from my.telegram.org
+#Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", ""))
 
-# Your API Hash from my.telegram.org
+#Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
 
-# Your db channel Id
+#Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
-# OWNER ID
+#OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 
-# Port
+#Port
 PORT = os.environ.get("PORT", "8080")
 
-# Database 
+#Database 
 DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
-# Force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", "-1002321045467,-1002246418579")
-FORCE_SUB_CHANNEL = [int(channel_id) for channel_id in FORCE_SUB_CHANNEL.split(",") if channel_id]
-
-# Join request enable
-JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", "true").lower() == "true"
+#force sub channel id, if you want enable force sub
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", None)
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-# Start message
-START_PIC = os.environ.get("START_PIC", "")
+#start message
+START_PIC = os.environ.get("START_PIC","")
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
-    ADMINS = []
+    ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
         ADMINS.append(int(x))
 except ValueError:
-    raise Exception("Your Admins list does not contain valid integers.")
+        raise Exception("Your Admins list does not contain valid integers.")
 
-# Force sub message 
+#Force sub message 
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
 
-# Set your Custom Caption here, Keep None for Disable Custom Caption
+#set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
-# Set True if you want to prevent users from forwarding files from bot
+#set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
 # Auto delete time in seconds.
@@ -60,7 +59,7 @@ AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "0"))
 AUTO_DELETE_MSG = os.environ.get("AUTO_DELETE_MSG", "This file will be automatically deleted in {time} seconds. Please ensure you have saved any necessary content before this time.")
 AUTO_DEL_SUCCESS_MSG = os.environ.get("AUTO_DEL_SUCCESS_MSG", "Your file has been successfully deleted. Thank you for using our service. ✅")
 
-# Set true if you want Disable your Channel Posts Share button
+#Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
