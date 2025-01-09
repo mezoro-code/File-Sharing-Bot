393d4ac742763735d6b971aa69f3522d88e154c2
mezoro-code <sologameacg@gmail.com>
2025-01-09 20:43:51 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE
 import pyrogram.utils
 
 pyrogram.utils.MIN_CHANNEL_ID = -1002321045467
+pyrogram.utils.MIN_CHANNEL_ID = -1002359990788
 ascii_art = """
 ░█████╗░░█████╗░██████╗░███████╗██╗░░██╗██████╗░░█████╗░████████╗███████╗
 ██╔══██╗██╔══██╗██╔══██╗██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗╚══██╔══╝╚════██║
